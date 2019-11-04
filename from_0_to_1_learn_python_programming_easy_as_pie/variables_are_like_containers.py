2b98ea56d9b1a21cac3a7fe6c6a7b3c8f799c191
CodyBrettWatson <cody.brett.watson@gmail.com>
2019-11-04 16:25:16 -0600

    updated comments

--- from_0_to_1_learn_python_programming_easy_as_pie/variables_are_like_containers.py
+++ from_0_to_1_learn_python_programming_easy_as_pie/variables_are_like_containers.py
@@ -2,17 +2,12 @@ radius_of_circle = 5
 pi = 3.14
 area_of_circle = pi * (radius_of_circle^2)
 nice_string = "If a circle has radius " + str(radius_of_circle) + " units, then its area is " + str(area_of_circle) + " units."
+# the `+` acts as a concatenation operator and allows for different variables to be combined together in a string. Non-string types
+# can't be added together so we convert the numeric type to a string type with `str()`. 
 print(nice_string)
 
-# We created 4 variables: radius_of_circle, pi,
-# area_of_circle, and nice_string.
+# We created 4 variables: radius_of_circle, pi, area_of_circle, and nice_string.
 # radius_of_circle and pi are both numeric variables
-# area_of_circle while producing a numeric type was
-# created by combining the variables radius_of_circle
-# and pi.
-# Then created nice_string which is a string variable,
-# which here is a combination of numeric(formatted as 
-# strings) and string types.
-# Lastly, printed the nice_string variable so the when
-# the code is run through a terminal or in a cell there
-# will be an output.
+# area_of_circle while producing a numeric type was created by combining the variables radius_of_circle and pi.
+# Then created nice_string which is a string variable, which here is a combination of numeric(formatted as strings) and string types.
+# Lastly, printed the nice_string variable so the when the code is run through a terminal or in a cell there will be an output.
